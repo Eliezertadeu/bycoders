a8a56a9b706695c2dda2e02ffd8ac929b5ba278b
Eliezertadeu <silva.eliezertadeu@gmail.com>
2021-08-25 19:00:37 -0300

    created ReadFileResource

---
+++ finance/src/main/java/com/bycorders/finance/resource/ReadFileResource.java
@@ -0,0 +1,10 @@
+package com.bycorders.finance.resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/file")
+public class ReadFileResource {
+
+}
