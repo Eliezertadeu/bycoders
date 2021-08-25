1bf29465446902e26dfa30bb6aa3a639f20a45bc
Eliezertadeu <silva.eliezertadeu@gmail.com>
2021-08-25 19:01:25 -0300

    created basic method to read file

--- finance/src/main/java/com/bycorders/finance/resource/ReadFileResource.java
+++ finance/src/main/java/com/bycorders/finance/resource/ReadFileResource.java
@@ -1,10 +1,19 @@
 package com.bycorders.finance.resource;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/file")
 public class ReadFileResource {
+
+    @PostMapping("/read")
+    public ResponseEntity<String> read(@RequestParam MultipartFile file) {
+        return ResponseEntity.ok().body(file.getOriginalFilename());
+    }
 
 }
