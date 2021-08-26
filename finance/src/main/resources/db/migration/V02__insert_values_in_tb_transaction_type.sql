19eed5fd196f21e6f00f58585e2c8c50d42a1a07
Eliezertadeu <silva.eliezertadeu@gmail.com>
2021-08-26 14:40:59 -0300

    created seeding to transactiontype

---
+++ finance/src/main/resources/db/migration/V02__insert_values_in_tb_transaction_type.sql
@@ -0,0 +1,9 @@
+INSERT INTO tb_transaction_type (tp_id, tp_description, tp_nature, tp_signal) VALUES (1, 'Débito', 'Entrada', '+');
+INSERT INTO tb_transaction_type (tp_id, tp_description, tp_nature, tp_signal) VALUES (2, 'Boleto', 'Saída', '-');
+INSERT INTO tb_transaction_type (tp_id, tp_description, tp_nature, tp_signal) VALUES (3, 'Financiamento', 'Saída', '-');
+INSERT INTO tb_transaction_type (tp_id, tp_description, tp_nature, tp_signal) VALUES (4, 'Crédito', 'Entrada', '+');
+INSERT INTO tb_transaction_type (tp_id, tp_description, tp_nature, tp_signal) VALUES (5, 'Recebimento Empréstimo', 'Entrada', '+');
+INSERT INTO tb_transaction_type (tp_id, tp_description, tp_nature, tp_signal) VALUES (6, 'Vendas', 'Entrada', '+');
+INSERT INTO tb_transaction_type (tp_id, tp_description, tp_nature, tp_signal) VALUES (7, 'Recebimento TED', 'Entrada', '+');
+INSERT INTO tb_transaction_type (tp_id, tp_description, tp_nature, tp_signal) VALUES (8, 'Recebimento DOC', 'Entrada', '+');
+INSERT INTO tb_transaction_type (tp_id, tp_description, tp_nature, tp_signal) VALUES (9, 'Aluguel', 'Saída', '-');
